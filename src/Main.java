f723823000c6be4432b58360f57c3e75e239ba64
Vanessa Carvalho <devanessa.carvalho@gmail.com>
2024-05-15 11:38:10 -0300

    feat: testando a api exchangerate

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("**********************************************");
+        System.out.println("Seja bem vindo/a ao Conversor de moedas ^^");
+
+        Scanner leituraDoTeclado = new Scanner(System.in);
+        System.out.println("Digite o valor que deseja converter: ");
+        int menu = leituraDoTeclado.nextInt();
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        String API_KEY = "e8c2a59f795108b991f65119";
+        String moeda = "USD";
+        String apiConversao = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/" + moeda;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiConversao))
+                .build();
+
+        HttpResponse <String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
     }
 }
