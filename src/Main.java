d5d64c44bbaa63bc583f3cadd1cfbbc2762e122a
Vanessa Carvalho <devanessa.carvalho@gmail.com>
2024-05-16 09:47:35 -0300

    feat: add conversão de dolar para reais

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -12,19 +16,35 @@ public class Main {
 
         Scanner leituraDoTeclado = new Scanner(System.in);
         System.out.println("Digite o valor que deseja converter: ");
-        int menu = leituraDoTeclado.nextInt();
+        float valor = leituraDoTeclado.nextInt();
+
+        String codigoMoedaOrigem = "USD";
+        String codigoMoedaDestino = "BRL";
+        String API_KEY = "e8c2a59f795108b991f65119";
+        String API_URL = "https://v6.exchangerate-api.com/v6/";
+        String apiConversao = API_URL + API_KEY + "/pair/" + codigoMoedaOrigem +'/' + codigoMoedaDestino;
 
         HttpClient client = HttpClient.newHttpClient();
 
-        String API_KEY = "e8c2a59f795108b991f65119";
-        String moeda = "USD";
-        String apiConversao = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/" + moeda;
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(apiConversao))
                 .build();
 
         HttpResponse <String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create();
+        
+        ExchangeRateApi exchangeRate = gson.fromJson(response.body(), ExchangeRateApi.class);
+
+        float taxaDeConversao = exchangeRate.conversionRate();
+        float valorConvertido = converterMoeda(valor, taxaDeConversao);
+
+        System.out.printf("O valor  $ %.2f (%s) =  $ %.2f (%s)%n", valor, codigoMoedaOrigem, valorConvertido, codigoMoedaDestino);
+    }
+
+    public static float converterMoeda(float valorOriginal, float taxaDeConversao) {
+        return valorOriginal * taxaDeConversao;
     }
 }
